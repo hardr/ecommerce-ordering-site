33229a4e0ec04380d0c6037a53ba65a62853e439
Ryan Harings <ryanharings@hotmail.com>
2016-08-15 16:39:51 -0600

    orders page complete, no js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,22 @@
 // jquery test
 $(document).on('ready', function() {
   console.log('sanity check!');
+
+  const ajaxGet = 'https://galvanize-eats-api.herokuapp.com/menu';
+  const ajaxPost = 'https://galvanize-eats-api.herokuapp.com/orders';
+
+  $.ajax({
+      method: 'GET',
+      dataType: 'json',
+      url: ajaxGet
+    }).done(function (results) {
+      var sprite = results.sprites.front_default;
+      var $img = $('<img src="' + sprite + '">');
+      $('.well').prepend($img);
+    });
+
 });
+// $('#target').val('op‌​tion:first');
 
 // es6 test
 const PI = 3.14;
